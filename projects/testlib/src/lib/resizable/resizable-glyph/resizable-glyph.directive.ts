5cb93e182d2c1848e0fcbc2f50358050f65cc085
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-04-13 09:36:49 +0200

    kinda working

--- projects/testlib/src/lib/resizable/resizable-glyph/resizable-glyph.directive.ts
+++ projects/testlib/src/lib/resizable/resizable-glyph/resizable-glyph.directive.ts
@@ -1,16 +1,17 @@
-import { Directive, Inject, Input } from '@angular/core';
-// import { ResizableComponent } from '../resizable/resizable.component'; // Project won't build (npm run build -- --configuration production)
+// import { Directive, Inject, Input } from '@angular/core';
+// // import { ResizableComponent } from '../resizable/resizable.component'; // Project won't build (npm run build -- --configuration production)
+import { Directive, Input } from '@angular/core';
 import type { ResizableComponent } from '../resizable/resizable.component'; // Unit tests don't work
 
-export type Position = 'top' | 'end' | 'bottom' | 'start';
+// // export type Position = 'top' | 'end' | 'bottom' | 'start';
 
 @Directive({
   selector: '[resizableGlyph]'
 })
 export class ResizableGlyphDirective {
 
-  constructor(@Inject('RESIZABLE') private resizable: ResizableComponent) { }
+//   constructor(@Inject('RESIZABLE') private resizable: ResizableComponent) { }
 
-  @Input('resizableGlyph') positions: Position[] = [];
+  @Input('resizableGlyph') positions: ('top' | 'end' | 'bottom' | 'start')[] = [];
 
 }
--- projects/testlib/src/lib/resizable/resizable/resizable.component.spec.ts
+++ projects/testlib/src/lib/resizable/resizable/resizable.component.spec.ts
@@ -1,7 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockDirective } from 'ng-mocks';
 import { ResizableComponent } from "./resizable.component";
+import { Directive, Inject, Input } from '@angular/core';
 import { ResizableGlyphDirective } from '../resizable-glyph/resizable-glyph.directive';
+
+// @Directive({ selector: '[resizableGlyph]' })
+// class ResizableGlyphMockDirective {
+//   constructor(@Inject('RESIZABLE') private resizable: ResizableComponent) { }
+//   @Input('resizableGlyph') positions: ('top' | 'end' | 'bottom' | 'start')[] = [];
+// }
 
 describe('BsResizableComponent', () => {
   let component: ResizableComponent;
@@ -15,9 +22,7 @@ describe('BsResizableComponent', () => {
         
         // Mock dependencies
         MockDirective(ResizableGlyphDirective),
-      ],
-      providers: [
-        // MockProvider('RESIZABLE', BsResizableComponent, 'useClass')
+        // ResizableGlyphMockDirective
       ]
     })
     .compileComponents();
